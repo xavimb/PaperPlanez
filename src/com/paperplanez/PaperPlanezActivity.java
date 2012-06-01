aee6e4869024766d53586d2d6c5f4209aa5a62be
Xavi Magrinyà <xavi.mb89@hotmail.com>
2012-06-01 23:53:48 +0300

    First commit

---
+++ src/com/paperplanez/PaperPlanezActivity.java
@@ -0,0 +1,14 @@
+package com.paperplanez;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PaperPlanezActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        System.out.println("esto lo peta!");
+    }
+}
